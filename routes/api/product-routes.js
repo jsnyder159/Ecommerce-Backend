9f42a303c54d97cee0d55352494ab4b66f8c1e29
Jason Snyder <jason.snyder159@gmail.com>
2022-03-26 12:06:22 -0400

    product-routes finished

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,20 +4,41 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // TODO find all products
+router.get('/', async (req, res) => {
+  // T*ODO find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category}, {model: Tag}],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // TODO find a single product by its `id`
+  // T*ODO find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}, {model: Tag}],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'There are no products with that id.'});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* TODO req.body should look like this...
+  /* req.body should look like this...
     {
       product_name: "Basketball",
       price: 200.00,
@@ -91,6 +112,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   //TODO  delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id."});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
