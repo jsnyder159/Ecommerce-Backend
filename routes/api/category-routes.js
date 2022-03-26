6db848f1b44455412dc4ad7b5ed0ebf837d9fde3
Jason Snyder <jason.snyder159@gmail.com>
2022-03-26 18:27:34 -0400

    built routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,89 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, Tag } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // TODO find all categories
+router.get('/', async (req, res) => {
+  // T*ODO find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{model: Product}],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // TODO find one category by its `id` value
+router.get('/:id', async (req, res) => {
+  // T*ODO find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}]
+    });
+
+    if(!categoryData) {
+      res.status(404).json({ message: "No Category with this id"})
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // TODO create a new category
+router.post('/', async (req, res) => {
+  // T*ODO create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // TODO update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  // T*ODO update a category by its `id` value
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          category_name: req.params.category_name,
+        },
+      });
+
+      if(!categoryData) {
+        res.status(404).json({ message: "No category with this id."})
+        return;
+      }
+      res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // TODO delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  // T*ODO delete a category by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!categoryData) {
+      res.status(404).json({ message: "No category with this id."})
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // T*ODO find a single tag by its `id`
   // be sure to include its associated Product data
   try { 
-    const tagData = await Tag.findbyPk(req.params.id, {
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product}]
     });
   
